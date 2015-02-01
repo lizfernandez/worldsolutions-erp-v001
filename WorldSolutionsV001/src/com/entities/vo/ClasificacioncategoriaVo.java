562f30f8b149d4e313cf1249a9909773cc4c16c0
lishzz08 <lishzz08@59b86ec6-1746-3c80-526a-0e0bde6b1cb3>
2015-02-01 14:47:39 +0000

    SE SUBE NUEVA DB

--- WorldSolutionsV001/src/com/entities/vo/ClasificacioncategoriaVo.java
+++ WorldSolutionsV001/src/com/entities/vo/ClasificacioncategoriaVo.java
@@ -1,29 +1,19 @@
 package com.entities.vo;
 
 import java.io.Serializable;
-import javax.persistence.*;
 
 
 /**
  * The persistent class for the clasificacioncategoria database table.
  * 
  */
-@Entity
-@Table(name="Clasificacioncategoria")
+
 public class ClasificacioncategoriaVo implements Serializable {
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(unique=true, nullable=false)
 	private int iClasificacionId;
-
 	private String cClasificacionCodigo;
-
 	private String cEstadoCodigo;
-
 	private String vClasificacionDescripcion;
-
     public ClasificacioncategoriaVo() {
     }
 
