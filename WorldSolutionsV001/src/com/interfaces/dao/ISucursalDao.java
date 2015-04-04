19a05ee1759688a489567c12c98b1fcd9d9bbcaf
diegogonzalesv <diegogonzalesv@gmail.com>
2015-04-04 04:16:28 +0000

    reportes contabilidad

--- WorldSolutionsV001/src/com/interfaces/dao/ISucursalDao.java
+++
@@ -1,9 +0,0 @@
-package com.interfaces.dao;
-
-import java.util.List;
-
-import com.entities.Sucursal;
-
-public interface ISucursalDao {
-	public List<Sucursal> listaSucursal(int pagInicio, int pagFin, Sucursal area);
-}
